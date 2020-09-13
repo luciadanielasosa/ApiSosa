2505fcb2e0322ff673e146cf2cb9daec6c6decdd
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-09-13 16:17:07 -0400

    se creo la solución ApiSosa

---
+++ ApiSosa/ApiSosa/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ApiSosa.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ApiSosa/ApiSosa/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiSosa.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
